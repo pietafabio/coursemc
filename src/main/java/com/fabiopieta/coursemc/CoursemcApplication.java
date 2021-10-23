7dffd8b0e49424dba52f218aebd0fff19c3a753c
Fabio Pieta <fabioxpieta@gmail.com>
2021-10-22 21:02:42 -0300

    Criando operacao de instanciacao

--- src/main/java/com/fabiopieta/coursemc/CoursemcApplication.java
+++ src/main/java/com/fabiopieta/coursemc/CoursemcApplication.java
@@ -1,13 +1,33 @@
-package com.fabiopieta.coursemc;
+package com.fabiopieta.coursemc; 
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fabiopieta.coursemc.domain.Categoria;
+import com.fabiopieta.coursemc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CoursemcApplication {
+public class CoursemcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escrirório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
