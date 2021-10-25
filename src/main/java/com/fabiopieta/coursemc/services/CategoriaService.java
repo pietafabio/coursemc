8c9703747a5c1637df62220492640fe1a4d3cf61
Fabio Pieta <fabioxpieta@gmail.com>
2021-10-24 21:38:42 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/fabiopieta/coursemc/services/CategoriaService.java
+++ src/main/java/com/fabiopieta/coursemc/services/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.fabiopieta.coursemc.domain.Categoria;
 import com.fabiopieta.coursemc.repositories.CategoriaRepository;
+import com.fabiopieta.coursemc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}
-
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 }
