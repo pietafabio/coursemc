bc229fb595ddeb3a4381bd7359f6afca786e5c80
Fabio Pieta <fabioxpieta@gmail.com>
2021-10-30 15:20:52 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/fabiopieta/coursemc/domain/Cliente.java
+++ src/main/java/com/fabiopieta/coursemc/domain/Cliente.java
@@ -16,7 +16,7 @@ import javax.persistence.OneToMany;
 
 import com.fabiopieta.coursemc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente implements Serializable {
@@ -30,7 +30,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
