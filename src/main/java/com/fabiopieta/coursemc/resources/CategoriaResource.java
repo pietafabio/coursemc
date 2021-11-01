af4944684335510a0f272cb0f5ab26215cd2f58f
Fabio Pieta <fabioxpieta@gmail.com>
2021-11-01 19:39:18 -0300

    Listando todas categorias com DTO

--- src/main/java/com/fabiopieta/coursemc/resources/CategoriaResource.java
+++ src/main/java/com/fabiopieta/coursemc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.fabiopieta.coursemc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fabiopieta.coursemc.domain.Categoria;
+import com.fabiopieta.coursemc.dto.CategoriaDTO;
 import com.fabiopieta.coursemc.services.CategoriaService;
 
 @RestController
@@ -46,4 +49,12 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
 }
--- src/main/java/com/fabiopieta/coursemc/services/CategoriaService.java
+++ src/main/java/com/fabiopieta/coursemc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.fabiopieta.coursemc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,9 +38,12 @@ public class CategoriaService {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
