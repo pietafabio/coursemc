3f36ec4256a4b6c23ade3999dab469b1263ad14e
Fabio Pieta <fabioxpieta@gmail.com>
2021-11-01 20:57:30 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/fabiopieta/coursemc/services/CategoriaService.java
+++ src/main/java/com/fabiopieta/coursemc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabiopieta.coursemc.domain.Categoria;
+import com.fabiopieta.coursemc.dto.CategoriaDTO;
 import com.fabiopieta.coursemc.repositories.CategoriaRepository;
 import com.fabiopieta.coursemc.services.exceptions.DataIntegrityException;
 import com.fabiopieta.coursemc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 		public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 			return repo.findAll(pageRequest);
+		}
+		
+		public Categoria fromDTO(CategoriaDTO objDto) {
+			return new Categoria(objDto.getId(), objDto.getNome());
 		}
 }
