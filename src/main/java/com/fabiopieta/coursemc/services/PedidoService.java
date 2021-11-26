694a3772d4d9e276aee566ed45c18d6f300b528e
Fabio Pieta <fabioxpieta@gmail.com>
2021-11-25 21:20:54 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/fabiopieta/coursemc/services/PedidoService.java
+++ src/main/java/com/fabiopieta/coursemc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fabiopieta.coursemc.domain.Cliente;
 import com.fabiopieta.coursemc.domain.ItemPedido;
 import com.fabiopieta.coursemc.domain.PagamentoComBoleto;
 import com.fabiopieta.coursemc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.fabiopieta.coursemc.domain.enums.EstadoPagamento;
 import com.fabiopieta.coursemc.repositories.ItemPedidoRepository;
 import com.fabiopieta.coursemc.repositories.PagamentoRepository;
 import com.fabiopieta.coursemc.repositories.PedidoRepository;
+import com.fabiopieta.coursemc.security.UserSS;
+import com.fabiopieta.coursemc.services.exceptions.AuthorizationException;
 import com.fabiopieta.coursemc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,6 +74,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
